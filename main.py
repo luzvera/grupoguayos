a540072df197dc0f07c5030b86b89fde553aba22
luzvera <luzveraa98@gmail.com>
2022-05-31 00:23:16 -0400

    integracion base de datos formulario

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,  render_template
 from config import config
 from models import db, Bache
 
@@ -16,6 +16,12 @@ def create_app(enviroment):
 enviroment = config['development']
 
 app = create_app(enviroment)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # Traer todos los baches
 @app.route('/api/v1/baches', methods=['GET'])
@@ -36,11 +42,11 @@ def get_bache(id):
 # Crear un bache
 @app.route('/api/v1/baches/', methods=['POST'])
 def create_bache():
-    body_diccionario = request.get_json(force=True)
+    body_diccionario = request.form
 
     bache = Bache.create(direccion=body_diccionario['direccion'], categoria=body_diccionario['categoria'], latitud=body_diccionario['latitud'], longitud=body_diccionario['longitud'], nombre_bache=body_diccionario['nombre_bache'], nombre_usuario=body_diccionario['nombre_usuario'])
 
-    return jsonify({'bache': bache.json()})
+    return render_template('index.html', bache= bache)
 
 # Borrar un bache
 @app.route('/api/v1/baches/<id>', methods=['DELETE'])
